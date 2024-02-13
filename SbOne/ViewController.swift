9de35ef9c4f799cf2eac33f07c9eef9ada784dbf
Burkay Atar <burkayatar5@gmail.com>
2024-02-13 03:35:17 +0300

    Initial Commit

---
+++ SbOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SbOne
+//
+//  Created by Burkay Atar on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

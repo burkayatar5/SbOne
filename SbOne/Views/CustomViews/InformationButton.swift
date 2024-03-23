f56c3e2ae0174e542006eb9c68d5a12679f948ed
Burkay Atar <burkayatar5@gmail.com>
2024-03-23 21:46:05 +0300

    Information Button created and button delegate protocol added

--- SbOne/Views/CustomViews/InformationButton.swift
+++ SbOne/Views/CustomViews/InformationButton.swift
@@ -5,4 +5,54 @@
 //  Created by Burkay Atar on 18.03.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol InformationButtonDelegate {
+    func informativeButtonTapped()
+}
+
+class InformationButton: UIButton {
+    
+    var informationButtonDelegate: InformationButtonDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureButton() {
+        translatesAutoresizingMaskIntoConstraints = false
+        //Button Config
+        var config = UIButton.Configuration.plain()
+        config.image = UIImage(systemName: "info.circle")
+        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 18, weight: .medium, scale: .large)
+        config.contentInsets = .zero
+        self.configuration = config
+        
+        
+        self.configurationUpdateHandler = { [weak self] _ in
+            guard let self = self else { return }
+            
+            var config = self.configuration
+            config?.imageColorTransformer = UIConfigurationColorTransformer { _ in
+                switch self.state {
+                case .highlighted:
+                    return UIColor.systemGray3
+                default:
+                    return UIColor.systemOrange
+                }
+            }
+            self.configuration = config
+        }
+        self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc
+    func buttonTapped() {
+        informationButtonDelegate?.informativeButtonTapped()
+    }
+}

48242aca3d1914d9d19a72d34dcd0772402e0627
Burkay Atar <burkayatar5@gmail.com>
2024-03-28 18:15:57 +0300

    InformationButton refactored

---
+++ SbOne/Views/CustomViews/DailyView.swift
@@ -0,0 +1,20 @@
+//
+//  DailyView.swift
+//  SbOne
+//
+//  Created by Burkay Atar on 25.03.2024.
+//
+
+import UIKit
+
+class DailyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- SbOne/Views/CustomViews/InformationButton.swift
+++ SbOne/Views/CustomViews/InformationButton.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
+enum InformationButtonType {
+    case image
+    case words
+}
+
 protocol InformationButtonDelegate {
-    func informativeButtonTapped()
+    func informativeButtonTapped(tag: Int)
 }
 
 class InformationButton: UIButton {
@@ -17,19 +22,28 @@ class InformationButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configureButton()
+        
+    }
+    
+    convenience init(buttonType: InformationButtonType, buttonName: String, buttonTag: Int) {
+        self.init(frame: .zero)
+        configureButton(buttonType: buttonType, buttonName: buttonName, buttonTag: buttonTag)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configureButton() {
+    private func configureButton(buttonType: InformationButtonType, buttonName: String, buttonTag: Int) {
         translatesAutoresizingMaskIntoConstraints = false
         //Button Config
         var config = UIButton.Configuration.plain()
-        config.image = UIImage(systemName: "info.circle")
-        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 18, weight: .medium, scale: .large)
+        if buttonType == .image {
+            config.image = UIImage(systemName: buttonName) //"info.circle"
+            config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 18, weight: .medium, scale: .large)
+        } else if buttonType == .words {
+            
+        }
         config.contentInsets = .zero
         self.configuration = config
         
@@ -48,11 +62,12 @@ class InformationButton: UIButton {
             }
             self.configuration = config
         }
+        self.tag = buttonTag
         self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
     @objc
     func buttonTapped() {
-        informationButtonDelegate?.informativeButtonTapped()
+        informationButtonDelegate?.informativeButtonTapped(tag: self.tag)
     }
 }
